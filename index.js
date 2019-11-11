aa2d2d7779f62276c8f1088ab6e4d077776c78a6
willyan <willyan.sergio.fidelis@gmail.com>
2019-11-11 04:09:24 +0000

    last

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const HOST = "0.0.0.0";
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("New req arrived!")
-  res.send("Hello World!");
+  console.log("New req arrived man!")
+  res.send("Hello World man!");
 });
 
 app.listen(PORT, HOST);
